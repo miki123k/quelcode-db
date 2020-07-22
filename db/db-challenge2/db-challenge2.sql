191dce1b09f3a02369c6f6ef642f4f795558e727
miki123k <mebs1dpanntly8uy@gmail.com>
2020-07-22 16:50:55 +0900

    外部キーの設定

--- db/db-challenge2/db-challenge2.sql
+++ db/db-challenge2/db-challenge2.sql
@@ -13,8 +13,8 @@ CREATE TABLE `users` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 CREATE TABLE `joins` (
-    `room_id` int(11) NOT NULL,
-    `user_id` int(11) NOT NULL,
+    `room_id` int(11) FOREIGN KEY(`room_id`) REFERENCES `chat_rooms`(`room_id`) NOT NULL,
+    `user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
     `joined_at` datetime NOT NULL,
     PRIMARY KEY(`room_id`,`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8
@@ -25,8 +25,8 @@ CREATE TABLE `chat_rooms` (
     `room_name` varchar(100) NOT NULL,
     `description` varchar(1000),
     `is_file_allowed` tinyint(1) DEFAULT 1 NOT NULL,
-    `create_user_id` int(11) NOT NULL,
-    `update_user_id` int(11) NOT NULL,
+    `create_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
+    `update_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
     `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
     `created_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
@@ -35,11 +35,11 @@ CREATE TABLE `chat_rooms` (
 
 CREATE TABLE `posts` (
     `post_id` int(11) NOT NULL AUTO_INCREMENT,
-    `room_id` int(11) NOT NULL,
+    `room_id` int(11) FOREIGN KEY(`room_id`) REFERENCES `chat_rooms`(`room_id`) NOT NULL,
     `message` varchar(1000) NOT NULL,
     `attachment_name`varchar(100),
-    `post_user_id` int(11) NOT NULL,
-    `update_user_id` int(11) NOT NULL,
+    `post_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
+    `update_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
     `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
     `posted_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
@@ -48,13 +48,13 @@ CREATE TABLE `posts` (
 
 CREATE TABLE `tasks` (
     `task_id` int(11) NOT NULL AUTO_INCREMENT,
-    `room_id` int(11) NOT NULL,
+    `room_id` int(11) FOREIGN KEY(`room_id`) REFERENCES `chat_rooms`(`room_id`) NOT NULL,
     `task` varchar(1000) NOT NULL,
-    `assigned_user_id` int(11) NOT NULL,
+    `assigned_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
     `limit` datetime,
     `is_completed` tinyint(1) DEFAULT 0 NOT NULL,
-    `create_user_id` int(11) NOT NULL,
-    `update_user_id` int(11) NOT NULL,
+    `create_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
+    `update_user_id` int(11) FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) NOT NULL,
     `is_deleted` tinyint(1) DEFAULT 0 NOT NULL,
     `created_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
