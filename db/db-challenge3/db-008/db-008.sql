e87503b3aa96fd7231bfb890a4186209ad3bfd31
miki123k <mebs1dpanntly8uy@gmail.com>
2020-07-30 11:53:59 +0900

    削除されているチャットルームが出ないよう修正

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -2,6 +2,6 @@ SELECT `room_name`,`name`,DATE_FORMAT(`joined_at`,'%Y-%m-%d') AS `joined_at`
 FROM `joins` AS J
     JOIN (SELECT `user_id`,`is_deleted`,`name` FROM `users` WHERE `is_deleted` = 0) AS U
     ON  J.`user_id` = U.`user_id`
-    JOIN `chat_rooms` AS C
+    JOIN (SELECT `room_id`,`is_deleted`,`room_name` FROM `chat_rooms` WHERE `is_deleted` = 0) AS C
     ON J.`room_id` = C.`room_id`
     ORDER BY `joined_at` ASC;
